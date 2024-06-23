7181f83375091729dfef94e44243502726dbbca4
adityadewhy <adityadewhy@gmail.com>
2024-06-23 16:52:58 +0530

    added basic routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,32 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono().basePath("/api/v1")
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+	return c.text('signup route')
 })
+
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+app.post('/api/v1/blog', (c) => {
+	return c.text('post blog route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('update blog route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get specific blog route')
+})
+
+app.get("/api/v1/blog/bulk",(c)=>{
+  return c.text("get all blogs")
+})
+
 
 export default app
